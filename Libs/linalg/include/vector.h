7d760ec7fa5f142db6db66f6f6cadcde952de049
rnatvey <rnatvey@gmail.com>
2025-10-19 01:37:00 +0300

    Кринжанул

--- Libs/linalg/include/vector.h
+++ Libs/linalg/include/vector.h
@@ -1,85 +1,80 @@
-#pragma once
+п»ї#pragma once
 #include <cmath>
 class vector2 {
 
 private:
 
-	double X;
-	double Y;
+	double X{};
+	double Y{};
 
-	inline void assign(vector2& vec) { this->X = vec.X; this->Y = vec.Y };
-	inline void assign(double a) { this->X = a; this->Y = a };
-	inline void assign(double x, double y) { this->X = x; this->Y = y };
+	inline void assign(const vector2& vec) { if (this != &vec) { this->X = vec.X; this->Y = vec.Y; } };
+	inline void assign(double a) { this->X = a; this->Y = a; };
+	inline void assign(double x, double y) { this->X = x; this->Y = y; };
 
 public:
 
-	//конструкторы
-	vector2() : {X = 0.0, Y = 0.0};
-	vector2(double a, double b) : {assign(a, b)};
-	vector2(double a) : {assign(a)};
-	vector2(vector2& vec) : {assign(vec)};
+	//пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+	vector2() : X{}, Y{} {}
+	vector2(double a, double b) :X{}, Y{} { assign(a, b); }
+	vector2(double a) : X{}, Y{} { assign(a); };
+	vector2(vector2& vec) : X{}, Y{} { assign(vec); };
 
-	//функции для работы с векторами
+	//пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
 
-	inline double VxV(const vector2& a, const  vector2& b) { return (a.X * b.X + a.Y * b.Y) } const;
-	inline double Norm(const vector2& a) { return std::sqrt(VxV(a,a)) } const;
-	inline void UniVector() { this /= Norm(this) };
-	inline vector2 UniVector(const vector2& vec) { vector2 res{}; res = vec / Norm(vec); return res } const;
+	inline double VxV(const vector2& a, const  vector2& b) { return (a.X * b.X + a.Y * b.Y); } const
+	inline double Norm(const vector2& a) { return std::sqrt(VxV(a, a)); } const
+	inline void UniVector() { assign(*this /= Norm(*this)); };
+	inline vector2 UniVector(const vector2& vec) { vector2 res{}; res = vec / Norm(vec); return res; } const
 
 	//**********************************************************************************
 
-	vector2 operator + (const vector2& add) const 
+	vector2 operator + (const vector2& add) const
 	{
-		X + add.X;
-		Y + add.Y;
+		return vector2(this->X + add.X, this->Y + add.Y);
 	}
 
 	vector2 operator - (const vector2& sub) const
 	{
-		X - sub.X;
-		Y - sub.Y;
+		return vector2(this->X - sub.X, this->Y - sub.Y);
 	}
 
 	vector2 operator / (double div) const
 	{
-		X / div;
-		Y / div;
+		return vector2(this->X / div, this->Y / div);
 	}
 
 	vector2 operator * (double mul) const
 	{
-		X * mul;
-		Y * mul;
+		return vector2(this->X * mul, this->Y * mul);
 	}
 
-	vector2 operator += (const vector2& add) const
+	vector2& operator += (const vector2& add)
 	{
-		X += add.X;
-		Y += add.Y;
+		vector2::assign(this->X + add.X, this->Y + add.Y);
+		return *this;
 	}
 
-	vector2 operator -= (const vector2& sub) const
+	vector2& operator -= (const vector2& sub)
 	{
-		X -= sub.X;
-		Y -= sub.Y;
+		vector2::assign(this->X - sub.X, this->Y - sub.Y);
+		return *this;
 	}
 
-	vector2 operator /= (double div) const
+	vector2& operator /= (double div)
 	{
-		X /= div;
-		Y /= div;
+		vector2::assign(*this / div);
+		return *this;
 	}
 
-	vector2 operator *= (double mul) const
+	vector2& operator *= (double mul)
 	{
-		X *= mul;
-		Y *= mul;
+		vector2::assign(*this * mul);
+		return *this;
 	}
 
-	vector2 operator = (const vector2& vec) const
+	vector2& operator = (const vector2& vec)
 	{
-		X = vec.X;
-		Y = vec.Y;
+		assign(vec);
+		return *this;
 	}
-
 };
--- src/src/main.cpp
+++ src/src/main.cpp
@@ -6,5 +6,4 @@ int main()
 {
 	std::cout << "111"<<std::endl;
 	std::cout << testing();
-	vector2 m = { 1.0,2.0 };
 }
