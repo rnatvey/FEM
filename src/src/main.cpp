a3d06cbb1437b50446e9bba9265c91869aab7b4b
rnatvey <rnatvey@gmail.com>
2026-02-16 00:03:55 +0300

    Импорт результатов

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -20,6 +20,8 @@
 #include "meshgenerator.h"
 #include "constants.h"
 #include "loadFunction.h"
+#include <fstream>
+
 using namespace Constants;
 
 void testStressCalculation() {
@@ -106,7 +108,7 @@ int main() {
         auto model = std::make_shared<FEModel>();
         auto meshGen = std::make_shared<MeshGenerator>(assembly);
         // Добавляем материалы
-        auto material = std::make_shared<Material>(1, 14.828, 0.4876, 1);
+        auto material = std::make_shared<Material>(1, 12.728, 0.48, 1);
         assembly->addMaterial(material);
         //
         Eigen::Vector2d center(0.0, 0.0);      // Центр колеса
@@ -119,7 +121,7 @@ int main() {
         double endAngle = startAngle + 60 * DEG_TO_RAD;                 // -135° (90° дуга)
 
         int radialLayers = 50;          // Слоев по толщине
-        int circumferentialNodes = 51;  // Узлов по окружности
+        int circumferentialNodes = 101;  // Узлов по окружности
 
         meshGen->createAnnulusSimple(center, innerRadius, outerRadius,
             startAngle, endAngle,
@@ -147,7 +149,7 @@ int main() {
         }
         std::cout << "   nodes fixed: " << innerNodesCount << std::endl;
 
-        double maxContactPressure = 1.3662;     // 1 МПа максимальное давление
+        double maxContactPressure = 858716.234090089e-6*1.4;     // 1 МПа максимальное давление
         double contactHalfWidth = outerRadius * std::sin(contactAngle);        //  полуширина контакта
         double contactCenterX = 0.0;           // Центр контакта
         auto hertzLoad = std::make_shared<LoadFunction>(LoadFunction::parabolicPressure(maxContactPressure, contactHalfWidth, contactCenterX));
@@ -234,7 +236,7 @@ int main() {
             //        << nodalDisp[i].x() << ", " << nodalDisp[i].y() << ")" << std::endl;
             //}
 
-            //// Выводим узловые напряжения
+            // //Выводим узловые напряжения
             //auto nodalStress = model->getNodalStresses();
             //std::cout << "Nodal Stresses (sigma_xx, sigma_yy, tau_xy):" << std::endl;
             //for (size_t i = 0; i < nodalStress.size(); ++i) {
@@ -260,6 +262,42 @@ int main() {
     //  //  std::cout << model->getElementStress(6, 0.0, 0.0) << std::endl;
     //   // testStressCalculation();
     //    
+
+        // далее попытка  портировать 
+        std::ofstream file("tire47.csv");
+        if (!file.is_open()) {
+            std::cerr << "cont open this file!" << std::endl;
+            return 1;
+        }
+        file << " nodeID, x, y, ux, uy, sx, sy, sxy\n";
+        std::vector<std::tuple<size_t, const double, const double, double, double, double,double,double>> data;
+        auto node_out = assembly->getNodes();
+        auto nodalDisp_out = model->getNodalDisplacements();
+        auto nodalStress_out = model->getNodalStresses();
+        for (size_t i = 0; i < node_out.size(); ++i) {
+            data.push_back(std::make_tuple(
+                i + 1,
+                node_out[i]->getCoordinates()(0),
+                node_out[i]->getCoordinates()(1),
+                nodalDisp_out[i].x(),
+                nodalDisp_out[i].y(),
+                nodalStress_out[i].x(),
+                nodalStress_out[i].y(),
+                nodalStress_out[i].z()
+            ));
+        }
+        for (const auto& row : data) {
+            file << std::get<0>(row) << ","
+                << std::get<1>(row) << ","
+                << std::get<2>(row) << ","
+                << std::get<3>(row) << ","
+                << std::get<4>(row) << ","
+                << std::get<5>(row) << ","
+                << std::get<6>(row) << ","
+                << std::get<7>(row) << "\n";
+        }
+        file.close();
+        std::cout << "data saved as tire47.csv" << std::endl;
     }
     catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
